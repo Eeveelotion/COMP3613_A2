4975a3b53b8b8c11ce876a562a72283eee614c03
Arvesh Gosine <arveshgosine@gmail.com>
2025-10-18 23:37:11 -0400

    minor fix to api in user

--- App/views/user.py
+++ App/views/user.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from App.controllers import (
     jwt_authenticate,
@@ -51,7 +51,7 @@ def create_employer():
 @user_views.route('/student', methods=['POST'])
 @jwt_required()
 def create_student():
-    if not is_staff:
+    if not is_staff(get_jwt_identity()):
         return jsonify({"Error": "Unauthorized"}), 403
     data = request.get_json()
     success, message = create_student(data["name"], data["password"])
@@ -61,7 +61,7 @@ def create_student():
 @user_views.route('/user', methods=['DELETE'])
 @jwt_required()
 def remove_user():
-    if not is_staff:
+    if not is_staff(get_jwt_identity()):
         return jsonify({"Error": "Unauthorized"}), 403
     data = request.get_json()
     success, message = delete_user(data["id"])
@@ -71,7 +71,7 @@ def remove_user():
 @user_views.route('/employer/<employer_id>', methods=['PUT'])
 @jwt_required()
 def update_employer(employer_id):
-    if not is_staff:
+    if not is_staff(get_jwt_identity()):
         return jsonify({"Error": "Unauthorized"}), 403
     data = request.get_json()
     success, message = update_employer_info(employer_id, data.get("new_password"))
@@ -81,7 +81,7 @@ def update_employer(employer_id):
 @user_views.route('/student/<student_id>', methods=['PUT'])
 @jwt_required()
 def update_student(student_id):
-    if not is_staff:
+    if not is_staff(get_jwt_identity()):
         return jsonify({"Error": "Unauthorized"}), 403
     data = request.get_json()
     success, message = update_student_info(student_id, data.get("new_password"))
